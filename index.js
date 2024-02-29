9ae5522479a1e5212e81ea4ca2e94cab2a0bd2f3
tauheed-12 <mohammadtauheed160@gmail.com>
2024-02-29 19:09:26 +0530

    changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app=express()
 
 const port=4000;
 app.get('/',(req,res)=>{
-    res.send("hli gulam")
+    res.send("hliknknks gulam")
 })
 app.get('/gym',(req,res)=>{
     res.send("yo man ")
